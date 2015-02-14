4c2fb0e49f2d70d409e18350feb8eab63e0b7406
carlomanf <carlomanf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-14 16:44:53 +0000

    First release on wordpress.org
    git-svn-id: https://plugins.svn.wordpress.org/foreign-language-font/trunk@1089952 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ foreign-language-font.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){var f=jQuery("body").css("font-family");f=f.split(",");for(var n=0;n<f.length;n++)f[n]=f[n].trim();f.splice(1,0,"'"+F.f+"'");f=f.join(", ");jQuery("body").css("font-family",f);});
---
+++ foreign-language-font.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Plugin Name: Foreign Language Font
+ * Description: Foreign Language Font allows you to display foreign language text in its own font.
+ * Author: Carlo Manf
+ * Plugin URI: http://carlomanf.id.au/products/foreign-language-font/
+ * Author URI: http://carlomanf.id.au/products/foreign-language-font/
+ * Version: 1.0
+ */
+
+//* Load the callback function
+if ( !function_exists( 'cm_settings_field_callback' ) )
+	require_once( 'lib/cm-settings-callbacks.php' );
+
+//* Initialise settings section
+add_action( 'admin_init', function() { //* flf_initialise_settings_section
+	if ( false === get_option( 'flf' ) )
+		add_option( 'flf' );
+
+	add_settings_section(
+		'flf', //* ID
+		'Foreign Language Font', //* title to be displayed
+		function() { //* callback
+			echo '<p>If you would like to display foreign language text in its own font, enter the font link and font name below.</p>';
+		},
+		'reading' //* settings page to add to
+	);
+
+	add_settings_field(
+		'url', //* ID
+		'<label for="flf[url]">Font Link</label>', //* label
+		'cm_settings_field_callback', //* callback
+		'reading', //* settings page to add to
+		'flf', //* section to add to
+		array(
+			'setting' => 'flf',
+			'field' => 'url',
+			'type' => 'text',
+			'description' => 'Enter the link to the font stylesheet.',
+			'filters' => array( 'esc_attr' )
+		)
+	);
+
+	add_settings_field(
+		'name', //* ID
+		'<label for="flf[name]">Font Name</label>', //* label
+		'cm_settings_field_callback', //* callback
+		'reading', //* settings page to add to
+		'flf', //* section to add to
+		array(
+			'setting' => 'flf',
+			'field' => 'name',
+			'type' => 'text',
+			'description' => 'Enter the name of the font.',
+			'filters' => array( 'esc_attr' )
+		)
+	);
+
+	register_setting( 'reading', 'flf' );
+} );
+
+//* Enqueue the font stylesheet and plugin helper script
+add_action( 'wp_enqueue_scripts', function() {
+	$options = get_option( 'flf' );
+	if ( empty( $options[ 'url' ] ) || empty( $options[ 'name' ] ) )
+		return;
+
+	wp_enqueue_style( 'foreign-language-font', esc_url( $options[ 'url' ] ) );
+
+	wp_enqueue_script( 'foreign-language-font', plugins_url( 'foreign-language-font.js', __FILE__ ), array( 'jquery' ) );
+	wp_localize_script( 'foreign-language-font', 'F', array( 'f' => esc_attr( $options[ 'name' ] ) ) );
+}, 60 );
